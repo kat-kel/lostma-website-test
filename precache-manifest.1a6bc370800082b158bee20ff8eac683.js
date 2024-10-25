4de82d2b3129b6ccb684d8505077878451877ac4
kat-kel <klarinet28@gmail.com>
2024-10-25 20:08:55 +0200

    Updates

---
+++ precache-manifest.1a6bc370800082b158bee20ff8eac683.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec3d941bfc0cc250bded2833f7546bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5247f7d67ab9e5d3c32d",
+    "url": "/static/js/2.f65cf444.chunk.js"
+  },
+  {
+    "revision": "636c0f1a68cc54196bc2",
+    "url": "/static/js/main.6ddea3c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
