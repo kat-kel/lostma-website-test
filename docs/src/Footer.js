071a79360f5b5203dcd3eff8cbba42b721c1f4cd
kat-kel <klarinet28@gmail.com>
2024-12-04 14:09:16 +0100

    update footer

--- docs/src/Footer.js
+++ docs/src/Footer.js
@@ -1,54 +1,103 @@
 import React from 'react';
+import { OverlayIcon } from './components/overlay';
 import logo from './images/LOGO_ERC-FLAG_EU_.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faBluesky } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faBluesky, faGithub, faReact, faBootstrap } from '@fortawesome/free-brands-svg-icons'
+
+
+const TopLeft = () => {
+    return (
+        <div className="col">
+            <div className='row'>
+                <div className='col-sm-2'><img src={logo} alt='ERC European Union logos' /></div>
+                <div className='col-sm-10'>
+                    <p>Funded by the European Research Council</p>
+                    <p>
+                        Horizon Europe ERC Grant number: <a target="_blank" rel="noreferrer" href="https://doi.org/10.3030/101117408">101117408</a>
+                    </p>
+                    <p>
+                        Views and opinions expressed are however those of the author(s) only and not necessarily reflect those of the European Union or the European Research Council. Neither the European Union nor the granting authority can be held responsible for them.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+const TopMiddle = () => {
+    return (
+        <div className="col-md-3">
+            <h5>Follow Us</h5>
+            <ul className='list-group'>
+                <li className='list-group-item'>
+                    <a target="_blank" rel="noreferrer" href="https://bsky.app/profile/lostma.bsky.social">
+                        <FontAwesomeIcon icon={faBluesky} />
+                    </a>
+                    <span> </span>
+                    <span>BlueSky</span>
+                </li>
+                <li className='list-group-item'><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</li>
+            </ul>
+        </div>
+    )
+}
+
+const UsedTools = ()  => {
+    return (
+        <span> {<OverlayIcon text={"React App"} faIcon={faReact} />} {<OverlayIcon text={"Boostrap"} faIcon={faBootstrap} />} {<OverlayIcon text={"GitHub Pages"} faIcon={faGithub} />}
+        </span>
+    )
+}
+
+const TopRight = () => {
+    return (
+        <div className='col-md-3'>
+            <h5>Website Credits</h5>
+            <p>
+                <ul className='list-group'>
+                    <li className='list-group-item'>Code Base: <a target="_blank" rel='noreferrer' href='https://github.com/kat-kel/lostma-website-test'>GitHub Repository</a></li>
+                    <li className='list-group-item'>Editor: <a target="_blank" rel='noreferrer' href='https://github.com/kat-kel'>Kelly Christensen</a></li>
+                    <li className='list-group-item'>Open-Source Tools: {<UsedTools />}</li>
+                </ul>
+            </p>
+        </div>
+    )
+}
+
+const TopColumns = () => {
+    return (
+        <>
+        <br />
+        <hr />
+        <div className="row">
+            <TopLeft />
+            <TopMiddle />
+            <TopRight />  
+        </div>
+        </>
+    )
+}
+
+const BottomLegal = () => {
+    return (
+        <>
+        <hr />
+        <div className="row">
+            <div className="col-md-3">
+                <p>&copy; 2024 lostma-erc | All rights reserved.</p>
+            </div>
+        </div>
+        </>
+    )
+}
 
 class Footer extends React.Component {
     render() {
         return (
             <footer className='footer mt-auto py-3'>
                 <div className="container">
-                    <br />
-                    <hr />
-                    <div className="row">
-                        <div className="col">
-                            <div className='row'>
-                                <div className='col-sm-2'><img src={logo} alt='ERC European Union logos' /></div>
-                                <div className='col-sm-8'>
-                                    <p>Funded by the European Research Council</p>
-                                    <p>
-                                        Horizon Europe ERC Grant number: <a target="_blank" rel="noreferrer" href="https://doi.org/10.3030/101117408">101117408</a>
-                                    </p>
-                                    <p>
-                                        Views and opinions expressed are however those of the author(s) only and not necessarily reflect those of the European Union or the European Research Council. Neither the European Union nor the granting authority can be held responsible for them.
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-3">
-                            <h5>Follow Us</h5>
-                            <ul className='list-group'>
-                                <li className='list-group-item'>
-                                    <a target="_blank" rel="noreferrer" href="https://bsky.app/profile/lostma.bsky.social">
-                                        <FontAwesomeIcon icon={faBluesky} />
-                                    </a>
-                                    <span> </span>
-                                    <span>BlueSky</span>
-                                </li>
-                                <li className='list-group-item'>
-                                    <FontAwesomeIcon icon={faLinkedin} />
-                                    <span> </span>
-                                    <span>LinkedIn</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <hr />
-                    <div className="row">
-                        <div className="col-md-6">
-                            <p>&copy; 2024 lostma-erc | All rights reserved.</p>
-                        </div>
-                    </div>
+                    <TopColumns />
+                    <BottomLegal />
                 </div>
             </footer>
         )
