9ee45890d17e0e1baa3c5cc51e61b78da65f505a
kat-kel <klarinet28@gmail.com>
2025-01-15 13:36:03 +0100

    add sections

--- src/app/releases/page.tsx
+++ src/app/releases/page.tsx
@@ -1,8 +1,25 @@
 export default function News() {
   return (
     <div>
+
       <h1>Releases</h1>
-        <p>Check back in for more information later.</p>
+      <p>Check back in for more information later.</p>
+      
+      <div className="container my-2 bg-gradient-to-l from-gray-100 dark:from-gray-800 p-4 rounded-xl">
+        <h2>Datasets</h2>
+        <p>...</p>
+      </div>
+
+      <div className="container my-2 bg-gradient-to-l from-gray-100 dark:from-gray-800 p-4 rounded-xl">
+        <h2>ML Models</h2>
+        <p>...</p>
+      </div>
+
+      <div className="container my-2 bg-gradient-to-l from-gray-100 dark:from-gray-800 p-4 rounded-xl">
+        <h2>Code</h2>
+        <p>...</p>
+      </div>
+
     </div>
   );
 }
